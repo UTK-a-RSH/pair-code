a7cfbf622c0459ccdde31a4ae471d9e715095c42
UtkarshLB216 <utkarsh2212gonda@gmail.com>
2024-11-10 23:37:34 +0530

    fix: Update database URL in drizzle.config to use environment variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./db/schema.ts",
   dbCredentials: {
-    url: "postgres://postgres:password@localhost:5432/postgres",
+    url: process.env.DATABASE_URL!,
   },
 
   verbose: true,
